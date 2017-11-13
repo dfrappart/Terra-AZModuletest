f916532fd20a0728d7ff8c45eb552b86f0465a15
David Frappart <david@frappart.com>
2017-11-13 19:28:47 +0100

    adding file for azure automation creds

---
+++ Modules/AzureAutomationCredentials/AzureAutomationCredentials.tf
@@ -0,0 +1,62 @@
+###################################################################################
+#This module allow the creation of credentials for Azure Automation account
+###################################################################################
+
+#Variable declaration for Module
+
+#The Automation Account name
+variable "AutoName" {
+  type    = "string"
+
+}
+
+#The Automation Account Location
+variable "AutoLocation" {
+  type    = "string"
+
+
+}
+
+#The resource group in which the Automation Account is located
+variable "AutoRG" {
+  type    = "string"
+
+}
+
+variable "EnvironmentTag" {
+  type    = "string"
+  default = "Poc"
+}
+
+variable "EnvironmentUsageTag" {
+  type    = "string"
+  default = "Poc usage only"
+}
+
+
+#Automation account creation
+
+resource "azurerm_automation_account" "Terra-AutoAccount" {
+
+    name                    = "${var.AutoName}"
+    location                = "${var.AutoLocation}"
+    resource_group_name     = "${var.AutoRG}"
+
+    sku {
+
+        name = "Basic"
+    }
+
+    tags {
+
+        environment = "${var.EnvironmentTag}"
+        usage       = "${var.EnvironmentUsageTag}"
+    }   
+}
+
+#Output
+
+output "Id" {
+
+  value = "${azurerm_automation_account.Terra-AutoAccount.id}"
+}
