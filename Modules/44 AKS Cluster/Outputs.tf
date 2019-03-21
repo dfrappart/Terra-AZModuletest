f801f08e7a35c881b605e5471847c6ba76a2199b
dfrappart <david@frappart.com>
2019-03-21 13:58:05 +0100

    changing output

--- Modules/44 AKS Cluster/Outputs.tf
+++ Modules/44 AKS Cluster/Outputs.tf
@@ -31,7 +31,7 @@ output "KubeFQDN" {
   value = "${azurerm_kubernetes_cluster.TerraAKS.fqdn}"
 }
 
-
+/*
 output "KubeAdminCFG" {
   value = "${azurerm_kubernetes_cluster.TerraAKS.kube_admin_config}"
 }
@@ -67,7 +67,7 @@ output "KubeAdminCFGRaw" {
   sensitive = true
   value = "${azurerm_kubernetes_cluster.TerraAKS.kube_admin_config_raw}"
 }
-
+*/
 
 output "KubeCfg" {
   sensitive = true
@@ -78,6 +78,35 @@ output "KubeCfg" {
 output "KubeCfgRaw" {
   sensitive = true
   value = "${azurerm_kubernetes_cluster.TerraAKS.kube_config_raw}"
+}
+
+
+output "KubeCFG_HostName" {
+  value = "${azurerm_kubernetes_cluster.TerraAKS.kubeconfig.0.host}"
+}
+
+output "KubeCFG_UserName" {
+  value = "${azurerm_kubernetes_cluster.TerraAKS.kube_config.0.username}"
+}
+
+output "KubeCFG_Password" {
+  sensitive = true
+  value = "${azurerm_kubernetes_cluster.TerraAKS.kube_config.0.password}"
+}
+
+output "KubeCFG_ClientCertificate" {
+  sensitive = true
+  value = "${azurerm_kubernetes_cluster.TerraAKS.kube_config.0.client_certificate}"
+}
+
+output "KubeCFG_ClientKey" {
+  sensitive = true
+  value = "${azurerm_kubernetes_cluster.TerraAKS.kube_config.0.client_key}"
+}
+
+output "KubeCFG_ClusCACert" {
+  sensitive = true
+  value = "${azurerm_kubernetes_cluster.TerraAKS.kube_config.0.cluster_ca_certificate}"
 }
 
 /*
