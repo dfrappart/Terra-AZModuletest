756fd4b0a0507ce942f50e151234310c12f17455
dfrappart <david@frappart.com>
2019-11-04 12:13:48 +0100

    Updating Module ACR for terraform 0.12.x

--- Modules/49 Azure Container Registry/Main.tf
+++ Modules/49 Azure Container Registry/Main.tf
@@ -6,21 +6,21 @@
 #Creating a vNet
 
 resource "azurerm_container_registry" "TerraACR" {
-  #count                         = "${var.MatricARCount}"
-  name                          = "${var.ACRName}"
-  resource_group_name           = "${var.ACRRG}"
-  location                      = "${var.ACRLocation}"
-  admin_enabled                 = "${var.IsAdminEnabled}"
-  #storage_account_id            = "${var.ACRSTOAID}" only for classic sku
-  sku                           = "${var.ACRSku}"
-  georeplication_locations      = "${var.ACRReplList}"
+  #count                         = var.MatricARCount
+  name                          = var.ACRName
+  resource_group_name           = var.ACRRG
+  location                      = var.ACRLocation
+  admin_enabled                 = var.IsAdminEnabled
+  #storage_account_id            = var.ACRSTOAID only for classic sku
+  sku                           = var.ACRSku
+  georeplication_locations      = var.ACRReplList
 
 
   tags {
-    Environment       = "${var.EnvironmentTag}"
-    Usage             = "${var.EnvironmentUsageTag}"
-    Owner             = "${var.OwnerTag}"
-    ProvisioningDate  = "${var.ProvisioningDateTag}"
+    Environment       = var.EnvironmentTag
+    Usage             = var.EnvironmentUsageTag
+    Owner             = var.OwnerTag
+    ProvisioningDate  = var.ProvisioningDateTag
   }
 }
 
--- Modules/49 Azure Container Registry/Outputs.tf
+++ Modules/49 Azure Container Registry/Outputs.tf
@@ -6,67 +6,67 @@
 #Output for the module
 
 output "Name" {
-  value = "${azurerm_container_registry.TerraACR.name}"
+  value = azurerm_container_registry.TerraACR.name
   description = "The name of the ACR"
 }
 
 
 output "RGName" {
-  value = "${azurerm_container_registry.TerraACR.resource_group_name}"
+  value = azurerm_container_registry.TerraACR.resource_group_name
   description = "The name of the RG containing the ACR"
 }
 
 
 output "Location" {
-  value = "${azurerm_container_registry.TerraACR.location}"
+  value = azurerm_container_registry.TerraACR.location
   description = "The location of the ACR"
 }
 
 output "AdminEnabled" {
-  value = "${azurerm_container_registry.TerraACR.admin_enabled}"
+  value = azurerm_container_registry.TerraACR.admin_enabled
   description = "The status of the admin account state, enabled or not"
 }
 
 /*
 output "Storage" {
-  value = "${azurerm_container_registry.TerraACR.storage_account_id}"
+  value = azurerm_container_registry.TerraACR.storage_account_id
   description = "The storage account underlying the ACR"
 }
 */
 
 output "SKu" {
-  value = "${azurerm_container_registry.TerraACR.sku}"
+  value = azurerm_container_registry.TerraACR.sku
   description = "The ACR SKU"
 }
 
 output "ReplicatedLocations" {
-  value = "${azurerm_container_registry.TerraACR.georeplication_locations}"
+  value = azurerm_container_registry.TerraACR.georeplication_locations
   description = "The Region in which the ACR is replicated"
 }
 
 
 output "Id" {
-  value = "${azurerm_container_registry.TerraACR.id}"
+  value = azurerm_container_registry.TerraACR.id
   description = "The Container Registry ID"
 }
 
 
 output "ACRLoginServer" {
-  value = "${azurerm_container_registry.TerraACR.login_server}"
+  value = azurerm_container_registry.TerraACR.login_server
   description = "The URL that can be used to log into the container registry"
 }
 
 
 
 output "ACRAdminName" {
-  value = "${var.IsAdminEnabled ? azurerm_container_registry.TerraACR.admin_username : "Admin not enabled"}"
+  value = var.IsAdminEnabled ? azurerm_container_registry.TerraACR.admin_username : "Admin not enabled"
   description = "The Region in which the ACR is replicated"
 }
 
 
 
 output "ACRAdminPassword" {
-  value = "${var.IsAdminEnabled ? azurerm_container_registry.TerraACR.admin_password : "Admin not enabled"}"
+  value = var.IsAdminEnabled ? azurerm_container_registry.TerraACR.admin_password : "Admin not enabled"
   description = "The Admin password of the ACR"
   sensitive = true
 }
