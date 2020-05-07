774b4c3ba6d758ec2eebea85c4494bbe4e6bc15d
David Frappart <david@teknews.cloud>
2020-05-08 01:21:10 +0200

    adding description on variables

--- Modules/44-2 AKS ClusterwithRBAC/Main.tf
+++ Modules/44-2 AKS ClusterwithRBAC/Main.tf
@@ -8,50 +8,35 @@
 resource "azurerm_kubernetes_cluster" "TerraAKSwithRBAC" {
 
   lifecycle {
-    ignore_changes = [
+    ignore_changes                    = [
       #Ignore change for node count since it is autoscaling
       default_node_pool[0].node_count
 
     ]
   }
   
-  name                = var.AKSClusName
-  location            = var.AKSLocation
-  resource_group_name = var.AKSRGName
-  node_resource_group = var.AKSNodesRG
-  /*
-  agent_pool_profile {
-    name                  = lower(var.AKSAgentPoolName)
-    count                 = var.AKSNodeCount
-    vm_size               = var.AKSNodeInstanceType
-    os_type               = var.AKSNodeOSType
-    os_disk_size_gb       = var.AKSNodeOSDiskSize
-    vnet_subnet_id        = var.AKSSubnetId
-    max_pods              = var.AKSMaxPods
-    type                  = var.AKSNodePoolType
-    availability_zones    = var.AKSAZ
-    enable_auto_scaling   = var.EnableAKSAutoScale
-    min_count             = var.MinAutoScaleCount
-    max_count             = var.MaxAutoScaleCount
-    
-   
-  }
-*/
+  name                                = var.AKSClusName
+  location                            = var.AKSLocation
+  resource_group_name                 = var.AKSRGName
+  node_resource_group                 = var.AKSNodesRG
+
+
   default_node_pool {
-    name                       = lower(var.AKSAgentPoolName)
-    vm_size                    = var.AKSNodeInstanceType
-    availability_zones         = var.AKSAZ
-    enable_auto_scaling        = var.EnableAKSAutoScale      
-    enable_node_public_ip      = var.EnableNodePublicIP        
-    max_pods                   = var.AKSMaxPods
-    os_disk_size_gb            = var.AKSNodeOSDiskSize   
-    type                       = var.AKSNodePoolType
-    vnet_subnet_id             = var.AKSSubnetId
-    min_count                  = var.MinAutoScaleCount
-    max_count                  = var.MaxAutoScaleCount
-    node_count                 = var.AKSNodeCount
-    node_labels                = var.AKSNodeLabels
-    node_taints                = var.AKSNodeTaints
+    name                              = lower(var.AKSAgentPoolName)
+    vm_size                           = var.AKSNodeInstanceType
+    availability_zones                = var.AKSAZ
+    enable_auto_scaling               = var.EnableAKSAutoScale      
+    enable_node_public_ip             = var.EnableNodePublicIP        
+    max_pods                          = var.AKSMaxPods
+    node_labels                       = var.AKSNodeLabels
+    node_taints                       = var.AKSNodeTaints
+    os_disk_size_gb                   = var.AKSNodeOSDiskSize   
+    type                              = var.AKSNodePoolType
+    vnet_subnet_id                    = var.AKSSubnetId
+    min_count                         = var.MinAutoScaleCount
+    max_count                         = var.MaxAutoScaleCount
+    node_count                        = var.AKSNodeCount
+
 
 
   }
@@ -59,68 +44,69 @@ resource "azurerm_kubernetes_cluster" "TerraAKSwithRBAC" {
   dns_prefix = lower(var.AKSprefix)
 
   service_principal {
-    client_id         = var.K8SSPId
-    client_secret     = var.K8SSPSecret
+    client_id                         = var.K8SSPId
+    client_secret                     = var.K8SSPSecret
 
   }
 
   addon_profile {
-    
-    /*
-    http_application_routing {
-      enabled = var.IshttproutingEnabled
-    }
-    */
+
 
     oms_agent {
-      enabled                     = var.IsOMSAgentEnabled
-      log_analytics_workspace_id  = lower(var.AKSLAWId)
+      enabled                         = var.IsOMSAgentEnabled
+      log_analytics_workspace_id      = lower(var.AKSLAWId)
     }
 
     kube_dashboard {
-      enabled = true
+      enabled                         = true
     }
+
+    /*
+    azure_policy {
+      enabled                         = true
+    }
+    */
   }
   
-  kubernetes_version                = var.KubeVersion
-  api_server_authorized_ip_ranges   = var.APIAccessList
-  enable_pod_security_policy        = var.EnablePodPolicy
+  kubernetes_version                  = var.KubeVersion
+  api_server_authorized_ip_ranges     = var.APIAccessList
+  enable_pod_security_policy          = var.EnablePodPolicy
 
   linux_profile {
-    admin_username  = var.AKSAdminName
+    admin_username                    = var.AKSAdminName
 
     ssh_key {
-      key_data      = var.PublicSSHKey
+      key_data                        = var.PublicSSHKey
     }
   }
 
   network_profile {
-    network_plugin        = "azure"
-    network_policy        = var.NetworkPolicyPlugin
-    dns_service_ip        = cidrhost(var.AKSSVCCIDR, var.AKSDNSSVCIPModfier)
-    docker_bridge_cidr    = var.AKSDockerBridgeCIDR
-    service_cidr          = var.AKSSVCCIDR
-    load_balancer_sku     = var.AKSLBSku
+    network_plugin                    = "azure"
+    network_policy                    = var.NetworkPolicyPlugin
+    dns_service_ip                    = cidrhost(var.AKSSVCCIDR, var.AKSDNSSVCIPModfier)
+    docker_bridge_cidr                = var.AKSDockerBridgeCIDR
+    service_cidr                      = var.AKSSVCCIDR
+    load_balancer_sku                 = var.AKSLBSku
 
   }
 
   role_based_access_control {
-    enabled               = true
+    enabled                           = true
 
     azure_active_directory {
-      client_app_id       = var.AADCliAppId
-      server_app_id       = var.AADServerAppId
-      server_app_secret   = var.AADServerAppSecret
-      tenant_id           = var.AADTenantId
+      client_app_id                   = var.AADCliAppId
+      server_app_id                   = var.AADServerAppId
+      server_app_secret               = var.AADServerAppSecret
+      tenant_id                       = var.AADTenantId
     }
 
   }
 
   tags = {
-    Environment           = var.EnvironmentTag
-    Usage                 = var.EnvironmentUsageTag
-    Owner                 = var.OwnerTag
-    ProvisioningDate      = var.ProvisioningDateTag
+    Environment                       = var.EnvironmentTag
+    Usage                             = var.EnvironmentUsageTag
+    Owner                             = var.OwnerTag
+    ProvisioningDate                  = var.ProvisioningDateTag
   }
 }
 
--- Modules/44-2 AKS ClusterwithRBAC/Variables.tf
+++ Modules/44-2 AKS ClusterwithRBAC/Variables.tf
@@ -1,141 +1,256 @@
 ##############################################################
-#This module allows the creation of a vNEt
+#AKS Module
 ##############################################################
 
+
+##############################################################
 #Variable declaration for Module
 
 variable "AKSClusName" {
-  type = string
-  default = "TerraAkSClus"
+  type                  = string
+  default               = "TerraAkSClus"
+  description           = "The name of the cluster"
 }
 
 variable "AKSLocation" {
-  type = string
+  type                  = string
+  description           = "The region of the AKS cluster"
 }
 
 variable "AKSRGName" {
-  type = string
+  type                  = string
+  description           = "The resource group name containing the AKS Control plane"
 }
+
+variable "AKSNodesRG" {
+  type                  = string
+  default               = null
+  description           = "Define the name of the resource group that will contains the nodes"
+}
+
+##############################################################
+# default node pool variables
 
 variable "AKSAgentPoolName" {
-  type = string
-  default = "TerraAKSAP"
-}
-
-variable "AKSNodeCount" {
-  type = string
-  default = "3"
+  type                  = string
+  default               = "aksdefaultpool"
+  description           = "The name of the default Agent pool"
 }
 
 
 variable "AKSNodeInstanceType" {
-  type = string
-  default = "Standard_DS2_v2"
+  type                  = string
+  default               = "Standard_DS2_v2"
+  description           = "The node instance type"
 }
 
+variable "AKSAZ" {
+  type                  = list(string)
+  default               = null
+  description           = "Thelist of the AZ targeted by the node pool"
+}
+
+variable "EnableAKSAutoScale" {
+  type                  = string
+  default               = null
+  description           = "Define if the AKS Cluster node pool will autoscale or not"
+}
+
+variable "EnableNodePublicIP" {
+  type                  = string
+  default               = null
+  description           = "Are Public IP allowed on the nodes in the node pool, default to false"
+}
+
+variable "AKSMaxPods" {
+  type                  = string
+  default               = 100
+  description           = "The maximum number of pods per node"
+}
+
+
+
+variable "AKSNodeOSDiskSize" {
+  type                  = string
+  default               = 30
+  description           = "The size of the OS Disk for the node"
+}
+
+variable "AKSNodePoolType" {
+  type                  = string
+  default               = "VirtualMachineScaleSets"
+  description           = "The type of node pool"
+}
+
+variable "AKSSubnetId" {
+  type                  = string
+  description           = "The subnet id in which lives the node pool, and thus the nodes"
+}
+
+
+
+variable "MinAutoScaleCount" {
+  type                  = string
+  default               = null
+}
+
+variable "MaxAutoScaleCount" {
+  type                  = string
+  default               = null
+}
+
+variable "AKSNodeCount" {
+  type                  = string
+  default               = "3"
+}
+
+
+/*
 variable "AKSNodeOSType" {
   type = string
   default = "Linux"
 }
 
-variable "AKSNodeOSDiskSize" {
-  type = string
-  default = "30"
-}
+*/
 
-variable "AKSSubnetId" {
-  type = string
-}
 
-variable "AKSMaxPods" {
-  type = string
-  default = "100"
-}
+
+
+##############################################################
+# dns related variables
+
 
 variable "AKSprefix" {
-  type = string
-  default = "terraaksdfr"
+  type                  = string
+  default               = "terraaksdfr"
+  description           = "DNS prefix for the AKS cluster"
 }
 
+##############################################################
+# service principal related variables
+
 variable "K8SSPId" {
-  type = string
+  type                  = string
+  description           = "The App Id of the AAD App used by AKS"
 }
 
 variable "K8SSPSecret" {
-  type = string
+  type                  = string
+  description           = "The App secret of the AAD App used by AKS"
 }
 
+##############################################################
+# variables for the add on block
+
 variable "AKSLAWId" {
-  type = string
-  description = "The Id of the associated logs analityc workspace, must be provided if var.IsOMSAgentEnabled is set to true"
-  default = null
+  type                  = string
+  description           = "The Id of the associated logs analityc workspace, must be provided if var.IsOMSAgentEnabled is set to true"
+  default               = null
 }
 
 variable "IsOMSAgentEnabled" {
-  type = string
-  default = null
+  type                  = string
+  default               = null
+  description           = "Define if Log Analytics extension is used or not"
 }
 
-variable "NetworkPolicyPlugin" {
-  type = string
-  default = "calico"
-  description = "The network policy plugin, can be either azure or calico"
-}
+
 
 variable "KubeVersion" {
-  type = string
-  default = null
+  type                  = string
+  default               = null
+  description           = "The kube version"
 }
+
+variable "APIAccessList" {
+  type                  = list(string)
+  default               = null
+  description           = "A list of Public IP authorized to connect to the control plane"
+}
+
+variable "EnablePodPolicy" {
+  type                  = string
+  default               = null
+  description           = "Is the pod policy enabled or not"
+}
+
+
+##############################################################
+# variables for the linux profile block
 
 variable "AKSAdminName" {
   type = string
-  default = "AKSAdmin"
+  default               = "aksadmin"
+  description           = "admin for the node"
 }
 
 variable "PublicSSHKey" {
-  type = string
+  type                  = string
+  description           = "ssh key for the node"
 }
 
+##############################################################
+# variables for the network profile block
+
+
+variable "NetworkPolicyPlugin" {
+  type                  = string
+  default               = "calico"
+  description           = "The network policy plugin, can be either azure or calico"
+}
+
+
 variable "AKSDNSSVCIPModfier" {
-  type = string
-  default = "10"
+  type                  = string
+  default               = "10"
 }
 
 variable "AKSDockerBridgeCIDR" {
-  type = string
-  default = "172.17.0.1/16"
+  type                  = string
+  default               = "172.17.0.1/16"
 }
 
 variable "AKSSVCCIDR" {
-  type = string
-  default = "172.19.0.0/16"
+  type                  = string
+  default               = "172.19.0.0/16"
 }
 
-variable "IshttproutingEnabled" {
-  type = string
-  default = null
+variable "AKSLBSku" {
+  type                  = string
+  default               = "Standard"
+  description           = "The sku for the lb, default to sandard, because it's so much better than the basic ^^, and anyway it is required for AZ usage"
 }
+
+
+##############################################################
+# variables for the rbac block
 
 variable "AADTenantId" {
-  type = string
+  type                  = string
+  description           = "The AAD tenant Id used for the RBAC AAD integration"
 
 }
 
 variable "AADServerAppSecret" {
-  type = string
+  type                  = string
+  description           = "The App server secret"
 
 }
 
 variable "AADServerAppId" {
-  type = string
+  type                  = string
+  description           = "The id of theapp used for AAD check"
 
 }
 
 variable "AADCliAppId" {
-  type = string
+  type                  = string
+  description           = "The id of the client app on which kube user authenticate"
 
 }
 
+##############################################################
+# Tags variables
 
 variable "EnvironmentTag" {
   type    = string
@@ -161,53 +276,11 @@ variable "ProvisioningDateTag" {
 #Last stuff available on AKS
 
 
-variable "AKSNodePoolType" {
-  type    = string
-  default = null
-}
 
-variable "AKSAZ" {
-  type    = list(string)
-  default = null
-}
 
-variable "EnableAKSAutoScale" {
-  type    = string
-  default = null
-}
 
-variable "EnableNodePublicIP" {
-  type = string
-  default = null
-}
 
-variable "MinAutoScaleCount" {
-  type    = string
-  default = null
-}
 
-variable "MaxAutoScaleCount" {
-  type    = string
-  default = null
-}
 
-variable "APIAccessList" {
-  type    = list(string)
-  default = null
-}
 
-variable "EnablePodPolicy" {
-  type    = string
-  default = null
-}
-
-variable "AKSNodesRG" {
-  type    = string
-  default = null
-}
-
-variable "AKSLBSku" {
-  type    = string
-  default = null
-}
 
