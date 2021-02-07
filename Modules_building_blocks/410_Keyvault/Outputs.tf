70a6081edea030271e14ae64d8616311707d422e
David Frappart <david.frappart-ext@rexel.com>
2021-02-07 02:29:47 +0100

    formating output

--- Modules_building_blocks/410_Keyvault/Outputs.tf
+++ Modules_building_blocks/410_Keyvault/Outputs.tf
@@ -6,41 +6,51 @@
 #VModule Output
 
 output "FullKVOutput" {
-  value = azurerm_key_vault.TerraKeyVault
+  value             = azurerm_key_vault.TerraKeyVault
+  sensitive         = true
 }
 output "Id" {
-  value = azurerm_key_vault.TerraKeyVault.id
+  value             = azurerm_key_vault.TerraKeyVault.id
+  sensitive         = true
 }
 
 output "Name" {
-  value = azurerm_key_vault.TerraKeyVault.name
+  value             = azurerm_key_vault.TerraKeyVault.name
+  sensitive         = false
 }
 
 output "Location" {
-  value = azurerm_key_vault.TerraKeyVault.location
+  value             = azurerm_key_vault.TerraKeyVault.location
+  sensitive         = false
 }
 
 output "RG" {
-  value = azurerm_key_vault.TerraKeyVault.resource_group_name 
+  value             = azurerm_key_vault.TerraKeyVault.resource_group_name
+  sensitive         = false 
 }
 
 output "SKU" {
-  value = azurerm_key_vault.TerraKeyVault.sku_name
+  value             = azurerm_key_vault.TerraKeyVault.sku_name
+  sensitive         = false
 }
 
 output "TenantId" {
-  value = azurerm_key_vault.TerraKeyVault.tenant_id
+  value             = azurerm_key_vault.TerraKeyVault.tenant_id
+  sensitive         = false
 }
 
 output "URI" {
-  value = azurerm_key_vault.TerraKeyVault.vault_uri
+  value             = azurerm_key_vault.TerraKeyVault.vault_uri
+  sensitive         = true
 }
 
 
 output "KeyVault_enabled_for_disk_encryption" {
-  value = azurerm_key_vault.TerraKeyVault.enabled_for_disk_encryption
+  value             = azurerm_key_vault.TerraKeyVault.enabled_for_disk_encryption
+  sensitive         = false
 }
 
 output "KeyVault_enabled_for_template_deployment" {
-  value = azurerm_key_vault.TerraKeyVault.enabled_for_template_deployment
+  value             = azurerm_key_vault.TerraKeyVault.enabled_for_template_deployment
+  sensitive         = false
 }
