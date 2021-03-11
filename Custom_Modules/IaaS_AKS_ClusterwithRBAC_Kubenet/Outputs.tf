c98a4e8c09dc243ecf7d0423a4f39b628f26e12d
David Frappart <david.frappart-ext@rexel.com>
2021-03-11 15:12:43 +0100

    updating resource name to match change in main.tf

--- Custom_Modules/IaaS_AKS_ClusterwithRBAC_Kubenet/Outputs.tf
+++ Custom_Modules/IaaS_AKS_ClusterwithRBAC_Kubenet/Outputs.tf
@@ -6,115 +6,115 @@
 #Output for the AKS module with RBAC enabled
 
 output "KubeName" {
-  value                                     = azurerm_kubernetes_cluster.TerraAKSwithRBAC.name
+  value                                     = azurerm_kubernetes_cluster.AKSRBACKubenet.name
 }
 
 output "KubeLocation" {
-  value                                     = azurerm_kubernetes_cluster.TerraAKSwithRBAC.location
+  value                                     = azurerm_kubernetes_cluster.AKSRBACKubenet.location
 }
 
 output "KubeRG" {
-  value                                     = azurerm_kubernetes_cluster.TerraAKSwithRBAC.resource_group_name
+  value                                     = azurerm_kubernetes_cluster.AKSRBACKubenet.resource_group_name
 }
 
 output "KubeVersion" {
-  value                                     = azurerm_kubernetes_cluster.TerraAKSwithRBAC.kubernetes_version
+  value                                     = azurerm_kubernetes_cluster.AKSRBACKubenet.kubernetes_version
 }
 
 
 output "KubeId" {
-  value                                     = azurerm_kubernetes_cluster.TerraAKSwithRBAC.id
+  value                                     = azurerm_kubernetes_cluster.AKSRBACKubenet.id
 }
 
 
 output "KubeFQDN" {
-  value                                     = azurerm_kubernetes_cluster.TerraAKSwithRBAC.fqdn
+  value                                     = azurerm_kubernetes_cluster.AKSRBACKubenet.fqdn
 }
 
 output "KubeAdminCFGRaw" {
   sensitive                                 = true
-  value                                     = azurerm_kubernetes_cluster.TerraAKSwithRBAC.kube_admin_config_raw
+  value                                     = azurerm_kubernetes_cluster.AKSRBACKubenet.kube_admin_config_raw
 }
 
 
 output "KubeAdminCFG" {
-  value                                     = azurerm_kubernetes_cluster.TerraAKSwithRBAC.kube_admin_config
+  value                                     = azurerm_kubernetes_cluster.AKSRBACKubenet.kube_admin_config
 }
 
 output "KubeAdminCFG_UserName" {
-  value                                     = azurerm_kubernetes_cluster.TerraAKSwithRBAC.kube_admin_config.0.username
+  value                                     = azurerm_kubernetes_cluster.AKSRBACKubenet.kube_admin_config.0.username
 }
 
 output "KubeAdminCFG_HostName" {
-  value                                     = azurerm_kubernetes_cluster.TerraAKSwithRBAC.kube_admin_config.0.host
+  value                                     = azurerm_kubernetes_cluster.AKSRBACKubenet.kube_admin_config.0.host
 }
 
 
 output "KubeAdminCFG_Password" {
   sensitive                                 = true
-  value                                     = azurerm_kubernetes_cluster.TerraAKSwithRBAC.kube_admin_config.0.password
+  value                                     = azurerm_kubernetes_cluster.AKSRBACKubenet.kube_admin_config.0.password
 }
 
 
 output "KubeAdminCFG_ClientKey" {
   sensitive                                 = true
-  value                                     = azurerm_kubernetes_cluster.TerraAKSwithRBAC.kube_admin_config.0.client_key
+  value                                     = azurerm_kubernetes_cluster.AKSRBACKubenet.kube_admin_config.0.client_key
 }
 
 
 output "KubeAdminCFG_ClientCertificate" {
   sensitive                                 = true
-  value                                     = azurerm_kubernetes_cluster.TerraAKSwithRBAC.kube_admin_config.0.client_certificate
+  value                                     = azurerm_kubernetes_cluster.AKSRBACKubenet.kube_admin_config.0.client_certificate
 }
 
 output "KubeAdminCFG_ClusCACert" {
   sensitive                                 = true
-  value                                     = azurerm_kubernetes_cluster.TerraAKSwithRBAC.kube_admin_config.0.cluster_ca_certificate
+  value                                     = azurerm_kubernetes_cluster.AKSRBACKubenet.kube_admin_config.0.cluster_ca_certificate
 }
 
 
 output "KubeControlPlane_SAI" {
   sensitive                                 = true
-  value                                     = azurerm_kubernetes_cluster.TerraAKSwithRBAC.identity
+  value                                     = azurerm_kubernetes_cluster.AKSRBACKubenet.identity
 }
 
 output "KubeControlPlane_SAI_PrincipalId" {
   sensitive                                 = true
-  value                                     = azurerm_kubernetes_cluster.TerraAKSwithRBAC.identity[0].principal_id
+  value                                     = azurerm_kubernetes_cluster.AKSRBACKubenet.identity[0].principal_id
 }
 
 output "KubeControlPlane_SAI_TenantId" {
   sensitive                                 = true
-  value                                     = azurerm_kubernetes_cluster.TerraAKSwithRBAC.identity[0].tenant_id
+  value                                     = azurerm_kubernetes_cluster.AKSRBACKubenet.identity[0].tenant_id
 }
 
 output "KubeKubelet_UAI" {
   sensitive                                 = true
-  value                                     = azurerm_kubernetes_cluster.TerraAKSwithRBAC.kubelet_identity
+  value                                     = azurerm_kubernetes_cluster.AKSRBACKubenet.kubelet_identity
 }
 
 output "KubeKubelet_UAI_ClientId" {
   sensitive                                 = true
-  value                                     = azurerm_kubernetes_cluster.TerraAKSwithRBAC.kubelet_identity[0].client_id
+  value                                     = azurerm_kubernetes_cluster.AKSRBACKubenet.kubelet_identity[0].client_id
 }
 
 output "KubeKubelet_UAI_ObjectId" {
   sensitive                                 = true
-  value                                     = azurerm_kubernetes_cluster.TerraAKSwithRBAC.kubelet_identity[0].object_id
+  value                                     = azurerm_kubernetes_cluster.AKSRBACKubenet.kubelet_identity[0].object_id
 }
 
 output "KubeKubelet_UAI_Id" {
   sensitive                                 = true
-  value                                     = azurerm_kubernetes_cluster.TerraAKSwithRBAC.kubelet_identity[0].user_assigned_identity_id
+  value                                     = azurerm_kubernetes_cluster.AKSRBACKubenet.kubelet_identity[0].user_assigned_identity_id
 }
 
 
 output "NodeRG" {
-  value                                     = azurerm_kubernetes_cluster.TerraAKSwithRBAC.node_resource_group
+  value                                     = azurerm_kubernetes_cluster.AKSRBACKubenet.node_resource_group
 }
 
 output "FullAKS" {
-  value                                     = azurerm_kubernetes_cluster.TerraAKSwithRBAC
+  value                                     = azurerm_kubernetes_cluster.AKSRBACKubenet
   sensitive                                 = true
 }
 
