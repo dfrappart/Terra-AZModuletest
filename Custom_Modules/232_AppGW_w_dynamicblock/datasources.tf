cb18a9c57fcf2802d34b42ee7f34db7d1fe3ca34
David Frappart <david.frappart-ext@rexel.com>
2021-03-13 01:47:39 +0100

    removing unused datasources

--- Custom_Modules/232_AppGW_w_dynamicblock/datasources.tf
+++ Custom_Modules/232_AppGW_w_dynamicblock/datasources.tf
@@ -8,16 +8,3 @@ data "azurerm_subscription" "current" {}
 #data source for the subscription setup logs features
 
 
-data "azurerm_resource_group" "RgSubLogs" {
-  name                        = var.RgSubLogName
-}
-
-data "azurerm_log_analytics_workspace" "LawSubLog" {
-  name                        = var.LawSubLogName
-  resource_group_name         = data.azurerm_resource_group.RgSubLogs.name
-}
-
-data "azurerm_storage_account" "STASubLog" {
-  name                        = var.StaSubLogName
-  resource_group_name         = data.azurerm_resource_group.RgSubLogs.name
-}
