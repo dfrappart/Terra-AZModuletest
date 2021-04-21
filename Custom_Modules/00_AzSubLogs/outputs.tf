fe6ca6f4986f7f3a6068c0bb3cd53a1e67034c1b
David Frappart <david.frappart-ext@rexel.com>
2021-04-21 02:14:09 +0200

    updating sensitivity for 0.15 compatibility

--- Custom_Modules/00_AzSubLogs/outputs.tf
+++ Custom_Modules/00_AzSubLogs/outputs.tf
@@ -24,72 +24,85 @@ output "RGLogId" {
 #Output for the log storage account
 
 output "STALogFull" {
-  value = azurerm_storage_account.STALog
-  sensitive = true
+  value                   = azurerm_storage_account.STALog
+  sensitive               = true
 }
 output "STALogName" {
-  value = azurerm_storage_account.STALog.name
+  value                   = azurerm_storage_account.STALog.name
 }
 
 output "STALogId" {
   value = azurerm_storage_account.STALog.id
+  sensitive               = true
 }
 
 output "STALogPrimaryBlobEP" {
   value = azurerm_storage_account.STALog.primary_blob_endpoint
+  sensitive               = true
 }
 
 output "STALogPrimaryQueueEP" {
-  value = azurerm_storage_account.STALog.primary_queue_endpoint
+  value                   = azurerm_storage_account.STALog.primary_queue_endpoint
+  sensitive               = true
 }
 
 output "STALogPrimaryTableEP" {
-  value = azurerm_storage_account.STALog.primary_table_endpoint
+  value                   = azurerm_storage_account.STALog.primary_table_endpoint
+  sensitive               = true
 }
 
 output "STALogPrimaryFileEP" {
-  value = azurerm_storage_account.STALog.primary_file_endpoint
+  value                   = azurerm_storage_account.STALog.primary_file_endpoint
+  sensitive               = true
 }
 
 output "STALogPrimaryAccessKey" {
-  value = azurerm_storage_account.STALog.primary_access_key
+  value                   = azurerm_storage_account.STALog.primary_access_key
+  sensitive               = true
 }
 
 output "STALogSecondaryAccessKey" {
-  value = azurerm_storage_account.STALog.secondary_access_key
+  value                   = azurerm_storage_account.STALog.secondary_access_key
+  sensitive               = true
 }
 
 output "STALogConnectionURI" {
-  value = azurerm_storage_account.STALog.primary_blob_connection_string
+  value                   = azurerm_storage_account.STALog.primary_blob_connection_string
+  sensitive               = true
 }
 
 ###################################################################
 #Output for the log analytics workspace
 
 output "SubLogAnalyticsFull" {
-  value = azurerm_log_analytics_workspace.SubLogAnalyticsWS
-  sensitive = true
+  value                   = azurerm_log_analytics_workspace.SubLogAnalyticsWS
+  sensitive               = true
 }
 output "SubLogAnalyticsWSId" {
-  value = azurerm_log_analytics_workspace.SubLogAnalyticsWS.id
+  value                   = azurerm_log_analytics_workspace.SubLogAnalyticsWS.id
+  sensitive               = true
 }
 
 output "SubLogAnalyticsWS_WSId" {
-  value = azurerm_log_analytics_workspace.SubLogAnalyticsWS.workspace_id
+  value                   = azurerm_log_analytics_workspace.SubLogAnalyticsWS.workspace_id
+  sensitive               = true
 }
 
 output "SubLogAnalyticsWS_Retention" {
-  value = azurerm_log_analytics_workspace.SubLogAnalyticsWS.retention_in_days
+  value                   = azurerm_log_analytics_workspace.SubLogAnalyticsWS.retention_in_days
 }
 
 output "SubLogAnalyticsWS_PrimaryAccessKey" {
-  value = azurerm_log_analytics_workspace.SubLogAnalyticsWS.primary_shared_key 
+  value                   = azurerm_log_analytics_workspace.SubLogAnalyticsWS.primary_shared_key
+  sensitive               = true 
 }
 
 output "SubLogAnalyticsWS_SecondaryAccessKey" {
-  value = azurerm_log_analytics_workspace.SubLogAnalyticsWS.secondary_shared_key  
+  value                   = azurerm_log_analytics_workspace.SubLogAnalyticsWS.secondary_shared_key 
+  sensitive               = true 
 }
 
 output "SubLogAnalyticsWS_PortalURL" {
-  value = azurerm_log_analytics_workspace.SubLogAnalyticsWS.portal_url  
+  value                   = azurerm_log_analytics_workspace.SubLogAnalyticsWS.portal_url  
+  sensitive               = true
 }
