6fd1f193b80edc0d303a4bfbffdcebd1fb321918
David Frappart <david.frappart-ext@rexel.com>
2021-10-11 15:13:10 +0200

    adding clus suffix in be rg name

--- Custom_Modules/IaaS_AKS_ClusterwithRBAC_AzureCNI/locals.tf
+++ Custom_Modules/IaaS_AKS_ClusterwithRBAC_AzureCNI/locals.tf
@@ -6,7 +6,7 @@ locals {
   AKSClusterName                        = "aks-${lower(var.AKSClusSuffix)}"
   AKSDefaultNodePoolName                = "aksnp0${lower(var.AKSClusSuffix)}"
   DNSPrefix                             = "aks${lower(var.Project)}${lower(var.Environment)}"
-  CustomNodeRGName                      = var.AKSNodesRG != "unspecified" ? var.AKSNodesRG : "rsg-${lower(var.Company)}${lower(var.CountryTag)}-${lower(var.Environment)}-${lower(var.Project)}-aksobjects" 
+  CustomNodeRGName                      = var.AKSNodesRG != "unspecified" ? var.AKSNodesRG : "rsg-${lower(var.Company)}${lower(var.CountryTag)}-${lower(var.Environment)}-${lower(var.Project)}-aksobjects${lower(var.AKSClusSuffix)}" 
   DefaultNodeRGName                     = null
 
   AGIC = {
