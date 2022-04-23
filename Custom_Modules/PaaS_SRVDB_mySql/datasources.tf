7efffd31725c3b8fe867604d429a54e264c6e44b
DavidFrappart <david@frappart.com>
2022-04-23 16:43:13 +0200

    adding data source curent subscription

---
+++ Custom_Modules/PaaS_SRVDB_mySql/datasources.tf
@@ -0,0 +1,13 @@
+#############################################################################
+#This file is used to define data source refering to Azure existing resources
+#############################################################################
+
+
+#############################################################################
+#data source for the subscription
+
+
+data "azurerm_subscription" "current" {}
+
+
+
