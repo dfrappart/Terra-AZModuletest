de4729b0a92dd84aa4817a46847688f7da0a738c
DavidFrappart <david@frappart.com>
2022-04-26 16:55:03 +0200

    Adding outputs

---
+++ Custom_Modules/PaaS_SRVDB_FlexiblePostgreSql/outputs.tf
@@ -0,0 +1,22 @@
+
+###################################################################################
+################################### Outputs ####################################
+###################################################################################
+
+output "FlexibleServer" {
+    value                           = azurerm_postgresql_flexible_server.PostGreSQLFlexServer
+    sensitive                       = true
+    description                     = "The full output of the Flexible Server"
+}
+
+output "FlexibleServerSubnetId" {
+    value                           = var.PSQLSubnetId == "unspecified" ? azurerm_subnet.psqlsubnet[0].id : var.PSQLSubnetId
+    sensitive                       = true
+    description                     = "The subnet id in which the flexible server lives"
+} 
+
+output "FlexibleServerPrivateDNSZoneId" {
+    value                           = var.PSQLPrivateDNSZoneId == "unspecified" ? azurerm_private_dns_zone.psqlflexdnszone[0].id : var.PSQLPrivateDNSZoneId
+    sensitive                       = true
+    description                  = "The Private DNS Zone Id in which the server is declared"        
+}
