7d2857f98c141afaa150a0ebf09780ce2aa4e965
dfrappart <david@frappart.com>
2023-09-18 11:50:01 +0200

    move uai config to specific file

--- Custom_Modules/232_AppGW_w_dynamicblock/agw.tf
+++ Custom_Modules/232_AppGW_w_dynamicblock/agw.tf
@@ -2,36 +2,9 @@
 ############################## Application Gateway ################################
 ###################################################################################
 
-###################################################################################
-#Creating AG User Assigned Managed identity
 
-resource "azurerm_user_assigned_identity" "AppGatewayManagedId" {
-  resource_group_name = var.TargetRG
-  location            = var.TargetLocation
-  name                = "uai-agw${var.AGWSuffix}"
-
-  tags = local.Tags
-}
 
 ###################################################################################
-#Creating access policy for uai
-
-resource "azurerm_key_vault_access_policy" "KeyVaultAccessPolicy01" {
-  key_vault_id = var.KVId
-  tenant_id    = data.azurerm_subscription.current.tenant_id
-  object_id    = azurerm_user_assigned_identity.AppGatewayManagedId.principal_id
-  certificate_permissions = [
-    "Get",
-    "List",
-  ]
-  key_permissions = []
-  secret_permissions = [
-    "Get",
-    "List",
-  ]
-}
-
-
 # Creating Azure Application Gateway
 
 resource "azurerm_application_gateway" "AGW" {
