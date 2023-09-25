a8426437004800a3d694892a5c9580f25a0cd392
dfrappart <david@frappart.com>
2023-09-25 17:16:26 +0200

    update terraform doc configuration

---
+++ Custom_Modules/IaaS_AKS_Cluster/.config/hclsamples/aks.tf
@@ -0,0 +1,48 @@
+######################################################################
+# Creating an Azure Kubernetes Cluster
+
+locals {
+
+  SitesConf = {
+    "Site 1" = {
+      SiteIdentifier                                = data.azurerm_key_vault_certificate.akscert.name 
+      AppGWSSLCertNameSite                          = data.azurerm_key_vault_certificate.akscert.name 
+      AppGwPublicCertificateSecretIdentifierSite    = data.azurerm_key_vault_secret.akscertsecret.id
+      HostnameSite                                  = "aks.teknews.cloud"
+      RoutingRulePriority                           = 1
+    }
+  }
+}
+
+
+module "AKS" {
+
+  #Module Location
+  source = "github.com/dfrappart/Terra-AZModuletest//Custom_Modules/IaaS_AKS_Cluster?ref=aksv1.2" 
+
+  #Module variable
+
+  AKSLocation                           = azurerm_resource_group.RG.location
+  AKSRGName                             = azurerm_resource_group.RG.name
+  AKSSubnetId                           = azurerm_subnet.subnet.id
+  AKSNetworkPlugin                      = "kubenet"
+  AKSNetPolProvider                     = "calico"
+  AKSClusSuffix                         = substr(replace(replace(each.key, ".", ""), "-", ""), 0, 12)
+  AKSIdentityType                       = "UserAssigned"
+  UAIIds                                = ["<UAI_ID>"]
+  PublicSSHKey                          = "<SSH_Key>"
+  AKSClusterAdminsIds                   = ["<AAD_Group_Object_Id>"]
+
+  LawLogId                              = "<>"
+  StaLogId                              = "<>"
+
+  LawOMSId                              = "<>"
+  IsOMSAgentEnabled                     = true
+
+  LawDefenderId                         = ""
+  IsDefenderEnabled                     = true
+
+  EnableDiagSettings                    = true
+
+
+}
