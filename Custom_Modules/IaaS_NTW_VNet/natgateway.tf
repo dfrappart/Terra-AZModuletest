53d924fc8ad7ef9b09a8a97f2bc5d32d968dd5d4
dfrappart <david@frappart.com>
2026-08-19 18:20:13 +0200

    fix association

--- Custom_Modules/IaaS_NTW_VNet/natgateway.tf
+++ Custom_Modules/IaaS_NTW_VNet/natgateway.tf
@@ -22,7 +22,7 @@ resource "azurerm_public_ip" "NatGwPubIp" {
 resource "azurerm_nat_gateway_public_ip_association" "NatPubIpAssociation" {
   count                = var.EnableNatGateway ? var.NatGatewayPubIpCount : 0
   nat_gateway_id       = azurerm_nat_gateway.NatGw[0].id
-  public_ip_address_id = azurerm_public_ip.NatGwPubIp[0].id
+  public_ip_address_id = azurerm_public_ip.NatGwPubIp[count.index].id
 }
 
 resource "azurerm_subnet_nat_gateway_association" "SubnetNatGwAssociation" {
